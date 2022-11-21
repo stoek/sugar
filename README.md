7ac2041f69016489d414374f0268f9bd3af0d919
Stan van der Vleuten <stanvandervleuten95@gmail.com>
2022-11-21 22:14:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# What is this?
+This is me experimenting writing my own interpeter in go, based on a book explaining just that. 
+I have tried to be orginal and called the lang it interprets "Sugar". 
+Which means that any syntax is a syntax sugar ;) 
+## Note
+Not that anyone will, cause it misses a gazillion features, but this should not be used for quite literally anything. 
---
+++ token/token.go
@@ -0,0 +1,62 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LSBRACE   = "{"
+	RSBRACE   = "}"
+	// Keywords
+	FUNCTION = "FUNCTION"
+	VAR      = "VAR"
+
+	RETURN = "RETURN"
+	IF     = "IF"
+	TRUE   = "TRUE"
+	FALSE  = "FALSE"
+	ELSE   = " ELSE"
+
+	// two characters
+	EQ     = "=="
+	NOT_EQ = "!="
+)
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"var":    VAR,
+	"if":     IF,
+	"return": RETURN,
+	"true":   TRUE,
+	"false":  FALSE,
+	"else":   ELSE,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
